6710ab6609ee1764aa3e01505ba7eb61f39a580d
Morpheus-23 <skyelah@icloud.com>
2021-12-13 21:24:26 +0000

    Set up structure of game functions

---
+++ assets/js/script.js
@@ -0,0 +1,65 @@
+function runGame() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function calculateCorrectAnswer() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongAnswer() {
+
+}
+
+function displayAdditionQuestion() {
+
+}
+
+function displaySubtractQuestion() {
+
+}
+
+function displayMulitplyQuestion() {
+
+}
+
+function displayDivisionQuestion() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function calculateCorrectAnswer() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongAnswer() {
+
+}
+
+function displayAdditionQuestion() {
+
+}
+
+function displaySubtractQuestion() {
+    
+}
+function displayMultiplyQuestion() {
+    
+}
+function displayDivisionQuestion() {
+    
+}
